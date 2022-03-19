d3f25c882eec495e3f636eef3432fe595636e53b
AbinayaRammohan <abinayarammohan2507@gmail.com>
2022-03-19 19:25:35 +0400

    Correction Done.

--- core/lead.py
+++ core/lead.py
@@ -4,5 +4,16 @@
 ##
 
 # Create Lead.
+
 def create_lead();
  print "Created Lead Successfully"
+
+# Update Lead..
+
+def update_lead();
+print "Updated Lead Successfully"
+
+# Delete Lead.
+
+def delete_lead();
+ print "Lead deleted Successfully"
