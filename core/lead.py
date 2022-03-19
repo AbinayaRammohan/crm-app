2be402ed2d6678a4da2e1dd70020a0dee04e560a
AbinayaRammohan <abinayarammohan2507@gmail.com>
2022-03-19 19:34:56 +0400

    Commiting the files.

---
+++ core/lead.py
@@ -0,0 +1,19 @@
+
+##
+# Lead Feauture Functionalities.
+##
+
+# Create Lead.
+
+def create_lead();
+ print "Created Lead Successfully"
+
+# Update Lead..
+
+def update_lead();
+print "Updated Lead Successfully"
+
+# Delete Lead.
+
+def delete_lead();
+ print "Lead deleted Successfully"
